c8d661fefeb459dd3d2375a5105c00226bfbfa1e
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2023-06-10 08:36:52 +0200

    Add breaking change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import logo2 from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo2} className="App-logo" alt="logo2" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
